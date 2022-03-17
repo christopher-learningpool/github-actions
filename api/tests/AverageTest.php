1cf9cda84444e00e11a3f4aff08f74342b32b864
Christopher McLaughlin <christopher@learningpool.com>
2022-03-17 21:42:51 +0000

    Test Github Actions

---
+++ api/tests/AverageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use api\Average;
+use PHPUnit\Framework\TestCase;
+
+class AverageTest extends TestCase {
+    protected $average;
+
+    public function setUp(): void {
+        $this->average = new Average();
+    }
+
+    public function testCalculationOfMean() {
+        $numbers = [3, 7, 6, 1, 5];
+        $this->assertEquals(4.4, $this->average->mean($numbers));
+    }
+
+    public function testCalculationOfMedian() {
+        $numbers = [3, 7, 6, 1, 5];
+        $this->assertEquals(5, $this->average->median($numbers));
+    }
+}
