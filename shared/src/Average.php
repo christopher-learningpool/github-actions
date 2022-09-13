14f0d3d23b0418332b328a46a53fd6a99b8b988f
Christopher McLaughlin <38788145+christopher-learningpool@users.noreply.github.com>
2022-09-13 19:06:52 +0100

    fix(GHA): Fix Github Action Releases. (#13)

--- shared/src/Average.php
+++ shared/src/Average.php
@@ -1,19 +1,15 @@
 <?php
 
-namespace core;
+namespace shared;
 
-use shared\SharedAverage;
-
-class Average {
+class SharedAverage {
     /**
      * Calculate the mean average
      * @param array $numbers Array of numbers
      * @return float Mean average
      */
     public function mean(array $numbers) {
-        $sharedaverage = new SharedAverage();
-
-        return $sharedaverage->mean($numbers);
+        return array_sum($numbers) / count($numbers);
     }
 
     /**
@@ -22,8 +18,14 @@ class Average {
      * @return float Median average
      */
     public function median(array $numbers) {
-        $sharedaverage = new SharedAverage();
-
-        return $sharedaverage->median($numbers);
+        sort($numbers);
+        $size = count($numbers);
+        if ($size % 2) {
+            return $numbers[$size / 2];
+        } else {
+            return $this->mean(
+                array_slice($numbers, ($size / 2) - 1, 2)
+            );
+        }
     }
 }
